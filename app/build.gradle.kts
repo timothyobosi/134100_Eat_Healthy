85b606767dfe4407ea8b911097ad85c62188cf09
Tim <timothy8@gmail.com>
2024-01-03 18:13:51 +0300

    initial commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,5 +75,11 @@ dependencies {
     //coroutines
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.4.1")
 
+    //retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.github.bumptech.glide:glide:4.12.0")
+    annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
+
 
 }
